685da8f96f40f6f230e17780c1834832805b09d6
Zyed007 <53857974+Zyed007@users.noreply.github.com>
2023-05-13 00:30:43 +0400

    Add files via upload

---
+++ Fire.js
@@ -0,0 +1,34 @@
+//import firebase from "firebase";
+//import "@firebase/firestore";
+
+//const firebaseConfig = {
+
+    //apiKey: "AIzaSyAdVNUGXZRXuNo-5cRbIDJ_7onA9ox6z48",
+    //authDomain: "zyedtodoapp.firebaseapp.com",
+    //projectId: "zyedtodoapp",
+    //storageBucket: "zyedtodoapp.appspot.com",
+    //messagingSenderId: "1025943685504",
+    //appId: "1:1025943685504:web:97ec807d14981132d39965",
+  //  measurementId: "G-5PJLZCDR1X"
+//}
+
+//class Fire {    
+    // init() {
+//        if(!firebase.apps.length) {
+  //          firebase.initializeApp(firebaseConfig);
+    //    }
+//
+  //      firebase.auth().onAuth().onAuthStateChanged (user =>{
+    //        if (user) {
+
+//            } else {
+  //              firebase 
+    //                .auth()
+      //              .signInAnonymously()
+        //            .catch(error => {});
+          //  }
+        //});
+    //}/
+//}
+
+export default Fire;
---
+++ tempData.js
@@ -0,0 +1,73 @@
+//Temp Data 
+ 
+export default tempData = [ 
+    {
+        id : 1,
+        name: "Plan a Trip",
+        color : "#24A6D9",
+        todos :[
+            {
+                title: "Book Flights",
+                completed :  false
+            },
+            {
+                title: "Passport Check",
+                completed :  true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed :  true
+            },
+            {
+                title: "Pack Luggage",
+                completed :  false    
+            }
+        ]
+    },
+    {
+        id :2,
+        name: "Errands",
+        color : "#8022D9",
+        todos :[
+            {
+                title: "Buy Milk",
+                completed :  false
+            },
+            {
+                title: "Plan weekend camping trip",
+                completed :  true
+            },
+            {
+                title: "Run 2 miles",
+                completed :  true
+            },
+            {
+                title: "Make a Video",
+                completed :  true    
+            },
+            {
+                title: "Take dog for a walk",
+                completed :  true    
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "Birthday Party",
+        color : "#595BD9",
+        todos :[
+            {
+                title: "Get Baloons",
+                completed :  false
+            },
+            {
+                title: "Send invitations",
+                completed :  true
+            },
+            {
+                title: "Walk Dinner Reservations",
+                completed :  true
+            }
+        ]
+    }
+]
